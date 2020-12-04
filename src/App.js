b19a78f8ee367f604ba2b6b2db549ec89c8f4f83
Ranjith kumar <ranjit4072@gmail.com>
2020-12-04 18:06:33 +0530

    Added basic example of create function from zustand.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from "react";
+import create from "zustand";
+
 import "./styles.css";
+
+let globalStore = create((set) => ({
+  counter: 0,
+  increment: () =>
+    set((state) => ({
+      counter: state.counter + 1
+    })),
+  decrement: () =>
+    set((state) => ({
+      counter: state.counter - 1
+    }))
+}));
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Counter />
     </div>
   );
 }
+
+const Counter = () => {
+  const { counter, increment, decrement } = globalStore();
+  return (
+    <div className="counter-container">
+      <div className="counter">The count is: {counter}</div>
+      <button className="increment" onClick={increment}>
+        +
+      </button>
+      <button className="decrement" onClick={decrement}>
+        -
+      </button>
+    </div>
+  );
+};
